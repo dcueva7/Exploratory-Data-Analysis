694b05d8e4ee51027e73c6512df76cb8347bb0a8
Daniel Cueva <dcueva@usc.edu>
2023-03-03 18:27:44 -0800

    added questions about which statistics to pursue

--- data.py
+++ data.py
@@ -9,4 +9,13 @@ file_name = r"C:\Users\dcueva\projects\Pandas\Exploratory-Data-Analysis\bitcoin.
 
 df = pd.read_csv(file_name)
 
-print(df)
+#Data Frame Columns: Timestamp, Open, High, Low, Close, Volume (BTC), Volume (Currency), Weighted Price
+
+print(df[['High', 'Close']].describe())
+
+#Questions:
+# 1. What time of the day did most transcations occur?
+# 2. What were the average opening, closing, highs, and lows each year?
+# 3. Which year had the most volume?
+# 4. Monthly volume trends?
+
