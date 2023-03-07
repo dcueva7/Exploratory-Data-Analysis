1eb69d2f0357e5733530a2bf771c119dfb500502
Daniel Cueva <dcueva@usc.edu>
2023-03-06 18:26:43 -0800

    Dropped missing values and created series for average high per month/year and average volume per month

--- data.py
+++ data.py
@@ -1,6 +1,10 @@
+# %%
 import pandas as pd
+import seaborn as sns
+from pygments import highlight
 
 file_name = r"C:\Users\dcueva\projects\Pandas\Exploratory-Data-Analysis\bitcoin.csv"
+sns.set_style("darkgrid")
 
 # ~~~~~Data Prep and Cleaning~~~~~~~
 # Load file using Pandas
@@ -11,7 +15,7 @@ df = pd.read_csv(file_name)
 
 #Data Frame Columns: Timestamp, Open, High, Low, Close, Volume (BTC), Volume (Currency), Weighted Price
 
-print(df[['High', 'Close']].describe())
+# print(df.describe())
 
 #Questions:
 # 1. What time of the day did most transcations occur?
@@ -19,3 +23,35 @@ print(df[['High', 'Close']].describe())
 # 3. Which year had the most volume?
 # 4. Monthly volume trends?
 
+#plot percentage of missing values per column
+percentage = (df.isna().sum().sort_values(ascending=False) / len(df))*100
+
+# percentage[percentage != 0].plot(kind='barh')
+
+
+#drop rows with at least one missing value
+df = df.dropna()
+
+#convert timestamp from unix to datetime
+df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='s')
+
+
+## Exploratory Analysis and Visualization (columns to analyze)
+# 1. High 
+# 2. Low 
+# 3. Volume (BTC)
+# 4. Open
+
+#get series for average high per month
+average_high_month = df.groupby(pd.PeriodIndex(df['Timestamp'], freq= "M"))['High'].mean()
+
+#get series for average high per year
+average_high_year = df.groupby(pd.PeriodIndex(df['Timestamp'], freq= "Y"))['High'].mean()
+
+#get series for volume per month
+volume_per_month = df.groupby(pd.PeriodIndex(df['Timestamp'], freq='M'))['Volume_(BTC)'].mean()
+
+
+volume_per_month.plot()
+
+# %%
